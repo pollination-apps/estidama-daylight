968a123d2956ac17486daac1db842e42b31459a2
Devang <devngc@gmail.com>
2022-07-13 17:19:16 -0400

    feat(*): working validation of occupied_areas

--- estidama.py
+++ estidama.py
@@ -3,7 +3,9 @@
 from __future__ import annotations
 import streamlit as st
 
-from typing import List, Dict
+import pandas as pd
+from typing import List, Dict, Tuple
+from enum import Enum
 
 from honeybee.room import Room
 from honeybee.aperture import Aperture
@@ -11,14 +13,44 @@ from honeybee.shade import Shade
 from honeybee.model import Model as HBModel
 
 
+class Program(Enum):
+    general = 'General'
+    retail = 'Retail'
+    residential = 'Residential'
+    school = 'School'
+
+
+def select_program() -> Program:
+    st.subheader('Program')
+    st.markdown('The program type will determine the threshold for compliance analysis.')
+
+    program = st.radio('Select program', options=[
+        name.value for name in Program])
+
+    for member in Program.__members__.values():
+        if member.value == program:
+            program = member
+            break
+
+    if program == Program.retail:
+        st.warning('All retail areas are excluded from achieving this credit.'
+                   ' If this is a mixed development project involving retail spaces,'
+                   ' make sure to NOT include those zones in your selection of'
+                   ' occupied areas.')
+
+    return program
+
+
 class OccupiedArea:
-    def __init__(self, room: Room, tolerance) -> None:
+    def __init__(self, room: Room, program: Program, tolerance: float) -> None:
         self._room = room
-        self._name = self._room.display_name
+        self._program = program
         self._tolerance = tolerance
+        self._name = self._room.display_name
         self._has_windows = False
         self._has_shades = False
         self._windows = []
+        self._warnings = []
         self._status = []
         self._extract_windows()
 
@@ -34,6 +66,10 @@ class OccupiedArea:
     def has_shades(self) -> bool:
         return self._has_shades
 
+    @property
+    def warnings(self) -> List[str]:
+        return self._warnings
+
     def _extract_windows(self) -> None:
         if self._room.exterior_aperture_area == 0:
             return
@@ -42,9 +78,10 @@ class OccupiedArea:
         for face in self._room.faces:
             if face.apertures:
                 for aperture in face.apertures:
-                    window = Window(aperture, self._tolerance)
+                    window = Window(aperture, self._program, self._tolerance)
                     self._windows.append(window.to_dict())
                     self._status.append(window.status)
+                    self._warnings.extend(window.warnings)
 
         if all(self._status):
             self._has_shades = True
@@ -58,24 +95,59 @@ class OccupiedArea:
         }
 
 
-def check_shade_width(shade: Shade, tolerance: float) -> bool:
-    left_edge, right_edge = shade.geometry.get_left_right_vertical_edges(tolerance)
-    closest_point = right_edge.closest_point(left_edge.midpoint)
-    distance = left_edge.midpoint.distance_to_point(closest_point)
+def check_shade_width(shade: Shade, tolerance: float) -> Tuple[bool, str]:
+    """Check that internal shade is not wider than 4 meters.
 
-    if distance <= 4.0:
-        return True
-    return False
+    args:
+        shade: The Honeybee shade object to run this check on.
+        tolerance: Tolerance for geometry calculation.
+
+    Returns:
+        A tuple of two elements:
+
+        -   True if the check passes. False otherwise.
+
+        -   A warning message as a string.
+    """
+    geo = shade.geometry
+
+    def _warning_1(shade: Shade):
+        return False, f'Shade {shade.display_name} is wider than 4 meters.'
+
+    def _warning_2(shade: Shade):
+        return False, f'The app could not validate shade {shade.display_name}. Make sure the width is less than 4 meters wide.'
+
+    if len(geo.vertices) <= 4:
+        for line in geo.boundary_segments:
+            dis = round(geo.center.distance_to_point(line.midpoint), 1)
+            if dis > 2.0:
+                return _warning_1(shade)
+    else:
+        if not shade.geometry.is_horizontal(tolerance):
+            output = shade.geometry.get_left_right_vertical_edges(tolerance)
+            if output:
+                left_edge, right_edge = output
+                closest_point = right_edge.closest_point(left_edge.midpoint)
+                distance = left_edge.midpoint.distance_to_point(closest_point)
+                if distance > 4.0:
+                    return _warning_1(shade)
+            else:
+                return _warning_2(shade)
+        else:
+            return _warning_2(shade)
+
+    return True, ''
 
 
-class Window:
-    def __init__(self, aperture: Aperture, tolerance: float) -> None:
+class Window(OccupiedArea):
+    def __init__(self, aperture: Aperture, program: Program, tolerance: float) -> None:
         self._aperture = aperture
+        self._program = program
         self._tolerance = tolerance
         self._has_external_shades = False
         self._has_internal_shades = False
         self._status = False
-        self._warning = None
+        self._warnings = []
         self._process_aperture()
 
     def _process_aperture(self):
@@ -83,15 +155,26 @@ class Window:
             self._has_external_shades = True
 
         if len(self._aperture.indoor_shades) > 0:
-            width_check = [check_shade_width(shade, self._tolerance)
-                           for shade in self._aperture.indoor_shades]
-            if all(width_check):
+            checks, warnings = [], []
+
+            for shade in self._aperture.indoor_shades:
+                width_check, warning = check_shade_width(shade, self._tolerance)
+                checks.append(width_check)
+                warnings.append(warning)
+
+            if all(checks):
                 self._has_internal_shades = True
             else:
-                self._warning = 'One or more of the internal shades are wider than 4 meters.'
+                for warning in warnings:
+                    if warning:
+                        self._warnings.append(warning)
 
         if self._has_external_shades or self._has_internal_shades:
             self._status = True
+        else:
+            if self._program == Program.school:
+                self._warnings.append(
+                    'For the program type of "School" shades are required.')
 
     @property
     def has_external_shades(self) -> bool:
@@ -105,14 +188,18 @@ class Window:
     def status(self) -> bool:
         return self._status
 
+    @property
+    def warnings(self) -> List[str]:
+        return self._warnings
+
     def to_dict(self) -> dict:
         output = {
             'name': self._aperture.display_name,
             'has_external_shades': self._has_external_shades,
             'has_internal_shades': self._has_internal_shades,
-            'warning': self._warning
+            'warning': self._warnings
         }
-        if not self._warning:
+        if not self._warnings:
             output.pop('warning')
 
         return output
@@ -122,7 +209,7 @@ def get_room_dict(hb_model: HBModel) -> Dict[str, Room]:
     return {room.display_name.lower(): room for room in hb_model.rooms}
 
 
-@st.cache()
+@st.cache(max_entries=1)
 def set_session_state_vals(room_names: List[str]):
     """Set Session State variables.
 
@@ -189,3 +276,47 @@ def get_occupied_rooms(hb_model: HBModel) -> List[Room]:
 
     return [room_dict[room]
             for room in st.session_state.unique_occupied_areas]
+
+
+def validate_rooms(occupied_rooms: List[Room], program: Program) -> None:
+
+    st.subheader('Validate Occupied Areas')
+    col1, col2 = st.columns(2)
+
+    with col1:
+        validate = st.checkbox('Validate')
+
+    with col2:
+        st.markdown(
+            'The app will check each occupied area for windows and shades (manual glare control devices).')
+
+    if validate:
+
+        if not occupied_rooms:
+            st.error('Add to occupied areas first.')
+            return
+
+        col3, col4 = st.columns(2)
+        tolerance = 0.01
+
+        with col3:
+            table_dict = {'Name': [], 'Windows': [], 'Shades': [], 'Warnings': []}
+            details = {}
+            for room in occupied_rooms:
+                occupied_area = OccupiedArea(room, program, tolerance)
+                table_dict['Name'].append(occupied_area.name)
+                table_dict['Windows'].append(occupied_area.has_windows)
+                table_dict['Shades'].append(occupied_area.has_shades)
+                table_dict['Warnings'].append(len(occupied_area.warnings))
+                details[occupied_area.name] = occupied_area.to_dict()
+
+            df = pd.DataFrame.from_dict(table_dict)
+            st.dataframe(df)
+
+        with col4:
+            name = st.text_input('Enter name to see warnings.')
+            if name:
+                if name not in details:
+                    st.error('Invalid name')
+                    return
+                st.write(details[name])
