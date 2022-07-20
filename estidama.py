8912cad600a2271d22050210dcd2584c15d08fe6
Devang <devngc@gmail.com>
2022-07-20 15:07:47 -0400

    refactor(*): start using streamlit tabs

--- estidama.py
+++ estidama.py
@@ -1,63 +1,14 @@
-"""Module capturing Estidama related objects and functions."""
+"""Module capturing Estidama objects."""
 
 from __future__ import annotations
-import streamlit as st
 
-import pandas as pd
-from typing import List, Dict, Tuple
-from enum import Enum
+from typing import List, Tuple
 
 from honeybee.room import Room
 from honeybee.aperture import Aperture
 from honeybee.shade import Shade
-from honeybee.model import Model as HBModel
 
-
-class Program(Enum):
-    """Building programs."""
-    general = 'General'
-    retail = 'Retail'
-    residential = 'Residential'
-    school = 'School'
-
-
-def relevant_definitions() -> None:
-    """Add relevant definitions from the Pearl Building Rating System."""
-
-    st.subheader('Relevant Definitions:')
-    st.markdown(
-        '1. Occupied area: Any internal space intended for sedentary occupancy.'
-    )
-    st.markdown(
-        '2. Mixed-use-development: A development that includes more than occupancy type'
-        ' - such as residential,'
-        'commercial, industrial, public or semi-public - within the same building, project'
-        'or site . The most common examples are a project with both commercial and'
-        'residential uses, or commercial and industrial uses. '
-    )
-
-
-def select_program() -> Program:
-    """Select and get the selected building program."""
-
-    st.subheader('Select Program')
-    st.markdown('The program type will determine the threshold for compliance analysis.')
-
-    program = st.radio('Select program', options=[
-        name.value for name in Program])
-
-    for member in Program.__members__.values():
-        if member.value == program:
-            program = member
-            break
-
-    if program == Program.retail:
-        st.warning('All retail areas are excluded from achieving this credit.'
-                   ' If this is a mixed development project involving retail spaces,'
-                   ' make sure to NOT include those zones in your selection of'
-                   ' occupied areas.')
-
-    return program
+from introduction import Program
 
 
 class OccupiedArea:
@@ -141,7 +92,11 @@ class OccupiedArea:
 def check_shade_width(shade: Shade, tolerance: float) -> Tuple[bool, str]:
     """Check that internal shade is not wider than 4 meters.
 
-    This check comes from Estidama.
+    This check comes from Estidama. If the internal shade has four or less vertices
+    the function finds the radial distance between the center of the face and the
+    mid-points of each of the edges. If the shade is vertical, the function find the
+    distance between the left most and the right most edges. The functions currently
+    does not validate a more complex shade geometry.
 
     args:
         shade: The Honeybee shade object to run this check on.
@@ -266,149 +221,3 @@ class Window:
             output.pop('warning')
 
         return output
-
-
-def get_room_dict(hb_model: HBModel) -> Dict[str, Room]:
-    """Get a dictionary of Room name to Room structure."""
-    return {room.display_name.lower(): room for room in hb_model.rooms}
-
-
-def hash_room(room: Room) -> dict:
-    """Function to help Streamlit hash a Honeybee Room."""
-    return {'name': room.display_name, 'volume': room.volume}
-
-
-@st.cache(max_entries=1, hash_funcs={Room: hash_room})
-def set_session_state_vals(room_dict: Dict[str, Room]):
-    """Set Session State variables.
-
-    This functions is created to take advantage of the the cache hit and cache miss
-    features of Streamlit. The function re-assigns two session state variables when
-    the list of argument changes.
-
-    args:
-        room_dict: A dictionary of Honeybee room names to Honeybee room structure.
-    """
-    st.session_state.occupied_areas = []
-    st.session_state.unique_occupied_areas = []
-
-
-def get_occupied_rooms(hb_model: HBModel) -> List[Room]:
-    """Get Rooms in the Honeybee model to be treated as Estidama Occupied Areas.
-
-    This function offers a UI for the users to select the Honeybee rooms to be treated
-    as occupied ares in the context of this app.
-
-    args:
-        hb_model: A Honeybee model.
-
-    returns:
-        A list of Honeybee Room objects.
-    """
-
-    room_dict = get_room_dict(hb_model)
-
-    st.subheader('Select Occupied Areas')
-    st.write('Either search using keywords or select the areas from'
-             ' the dropdown menu and add the occupied areas. Here, examples of'
-             ' a keyword are "meeting", "classroom", "corridor", etc.')
-
-    set_session_state_vals(room_dict)
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-        name = st.text_input('Search areas')
-        if name:
-            searched_areas = [room_name for room_name in room_dict
-                              if room_name.find(name) == 0]
-        else:
-            searched_areas = []
-
-    with col2:
-        chosen_areas = st.multiselect('Choose areas.',
-                                      options=list(room_dict.keys()))
-
-    staged_areas: List[str] = searched_areas + chosen_areas
-    st.write('Areas staged to be added to the occupied areas.')
-    st.write(staged_areas)
-
-    col3, col4 = st.columns(2)
-    with col3:
-        add_areas = st.button('Add to occupied areas', key='add rooms',
-                              help='Add staged areas to the occupied areas.')
-
-    with col4:
-        clear_areas = st.button(
-            'Clear occupied areas', key='clear occupied areas',
-            help='Reset occupied areas.')
-
-    if add_areas:
-        st.session_state.occupied_areas.extend(staged_areas)
-
-    st.session_state.unique_occupied_areas = list(
-        set(st.session_state.occupied_areas))
-
-    if clear_areas:
-        st.session_state.occupied_areas = []
-        st.session_state.unique_occupied_areas = []
-
-    st.write('Selected occupied areas')
-    st.write(st.session_state.unique_occupied_areas)
-
-    return [room_dict[room]
-            for room in st.session_state.unique_occupied_areas]
-
-
-def validate_rooms(rooms: List[Room], program: Program,
-                   tolerance: float) -> None:
-    """Validate the Honeybee Rooms selected as occupied areas.
-
-    The function will validate the room by looking at their apertures and shades.
-
-    args:
-        rooms: A list of Honeybee rooms to be treated as Occupied areas.
-        program : Selcted building program.
-        tolerance: A float representing the tolerance.
-    """
-
-    st.subheader('Validate Occupied Areas')
-    col1, col2 = st.columns(2)
-
-    with col1:
-        validate = st.checkbox('Validate')
-
-    with col2:
-        st.markdown(
-            'The app will check each occupied area for windows and shades'
-            ' (manual glare control devices).')
-
-    if validate:
-
-        if not rooms:
-            st.error('Add to occupied areas first.')
-            return
-
-        col3, col4 = st.columns(2)
-
-        with col3:
-            table_dict = {'Name': [], 'Windows': [], 'Shades': [], 'Warnings': []}
-            details = {}
-            for room in rooms:
-                occupied_area = OccupiedArea(room, program, tolerance)
-                table_dict['Name'].append(occupied_area.name)
-                table_dict['Windows'].append(occupied_area.has_windows)
-                table_dict['Shades'].append(occupied_area.has_shades)
-                table_dict['Warnings'].append(len(occupied_area.warnings))
-                details[occupied_area.name] = occupied_area.to_dict()
-
-            df = pd.DataFrame.from_dict(table_dict)
-            st.dataframe(df)
-
-        with col4:
-            name = st.text_input('Enter name to see warnings.')
-            if name:
-                if name not in details:
-                    st.error('Invalid name')
-                    return
-                st.write(details[name])
---
+++ introduction.py
@@ -0,0 +1,119 @@
+"""Introduction tab of the Estidama-daylight tab."""
+
+import streamlit as st
+from PIL import Image
+
+from enum import Enum
+from pathlib import Path
+from typing import Tuple, Union
+
+from honeybee.model import Model as HBModel
+from pollination_streamlit_io import get_hbjson
+
+from helper import write_hbjson
+from web import show_model
+
+
+class Program(Enum):
+    """Building programs."""
+    general = 'General'
+    retail = 'Retail'
+    residential = 'Residential'
+    school = 'School'
+
+
+def relevant_definitions() -> None:
+    """Add relevant definitions from the Pearl Building Rating System."""
+
+    st.subheader('Relevant Definitions:')
+    st.markdown(
+        '1. Occupied area: Any internal space intended for sedentary occupancy.'
+    )
+    st.markdown(
+        '2. Mixed-use-development: A development that includes more than occupancy type'
+        ' - such as residential,'
+        'commercial, industrial, public or semi-public - within the same building, project'
+        'or site . The most common examples are a project with both commercial and'
+        'residential uses, or commercial and industrial uses. '
+    )
+
+
+def select_program() -> Program:
+    """Select and get the selected building program."""
+
+    st.subheader('Select Program')
+    st.markdown('The program type will determine the threshold for compliance analysis.')
+
+    program = st.radio('Select program', options=[
+        name.value for name in Program])
+
+    for member in Program.__members__.values():
+        if member.value == program:
+            program = member
+            break
+
+    if program == Program.retail:
+        st.warning('All retail areas are excluded from achieving this credit.'
+                   ' If this is a mixed development project involving retail spaces,'
+                   ' make sure to NOT include those zones in your selection of'
+                   ' occupied areas.')
+
+    return program
+
+
+def introduction(target_folder: Path, host: str) -> Tuple[Program, Union[HBModel, None]]:
+    """UI for the Introduction tab of the Estidama app.
+
+    args:
+        target_folder: Path to the folder where all the data will be written.
+        host: A string conveying the environment inside which the app is running.
+
+    returns:
+        A tuple of two items;
+
+        -   program: A Program object representing the building type selected by the user.
+
+        -   hb_model: A Honeybee model object.
+    """
+
+    col1, col2 = st.columns([1, 5])
+
+    with col1:
+        image = Image.open('assets/images/estidama.png')
+        st.image(image, width=100)
+
+    with col2:
+        st.title('Estidama Daylight')
+
+    st.markdown('An app to check compliance for the Pearl Building Rating'
+                ' System(PBRS) LBi-7 Daylight & Glare credit.'
+                ' This app uses version 1.0, April 2010 of PBRS that can be'
+                ' accessed'
+                ' [here](https://pages.dmt.gov.ae/en/Urban-Planning/'
+                'Pearl-Building-Rating-System). ')
+
+    relevant_definitions()
+
+    program = select_program()
+
+    data = get_hbjson('upload-model')
+
+    if data:
+
+        model_data = data['hbjson']
+        hb_model = HBModel.from_dict(model_data)
+
+        if len(hb_model.rooms) == 0:
+            st.error('The uploaded model does not have any rooms (zones).')
+            return
+
+        if host == 'web':
+            st.markdown('Visually inspect the model to see if this is what you'
+                        ' wish to simulate.')
+
+            hbjson_path = write_hbjson(target_folder, hb_model)
+            show_model(hbjson_path, target_folder, key='model')
+    else:
+        hb_model = None
+
+    return program, hb_model
