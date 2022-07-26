82dcbf44be1b81b8e53c96b4ff62d24cc22f9f58
Devang <devngc@gmail.com>
2022-07-26 15:55:03 -0400

    feat(*): use lat & long to create CIE sky

--- simulation.py
+++ simulation.py
@@ -3,7 +3,9 @@
 
 import streamlit as st
 import time
+import folium
 
+from streamlit_folium import st_folium
 from typing import Union, Tuple
 from pathlib import Path
 from streamlit.uploaded_file_manager import UploadedFile
@@ -33,7 +35,8 @@ def get_epw(epw_data: UploadedFile, target_folder: Path) -> Union[EPW, None]:
     return epw
 
 
-def cie_sky(location: Location, month: int, day: int, hour: int, north: int) -> str:
+def cie_sky(latitude: float, longitude: float, month: int,
+            day: int, hour: int, north_angle: int, ground_reflectance: float) -> str:
     """Get representation of the CIE sky as a string.
 
     args:
@@ -46,13 +49,14 @@ def cie_sky(location: Location, month: int, day: int, hour: int, north: int) -> 
     returns:
         A string representation of the CIE clear sky with sun.
     """
-    cie = CIE.from_location(location, month, day, hour, north_angle=north)
+    cie = CIE.from_lat_long(latitude, longitude, 4, month,
+                            day, hour, 0, north_angle, ground_reflectance)
     return f'cie -alt {cie.altitude} -az {cie.azimuth} -type 0 -g 0.2'
 
 
 def create_job(hbjson_path: Path, api_client: ApiClient, owner: str, project: str,
-               name: str, description: str,
-               north: int, epw: EPW) -> NewJob:
+               name: str, description: str, latitude: float, longitude: float,
+               north_angle: int, ground_reflectance: float = 0.2) -> NewJob:
     """Create a Job to run a simulation on Pollination.
 
     args:
@@ -86,8 +90,8 @@ def create_job(hbjson_path: Path, api_client: ApiClient, owner: str, project: st
         argument['model'] = model_path
         argument['metric'] = 'illuminance'
         argument['radiance-parameters'] = '-ab 7'
-        argument['sky'] = cie_sky(
-            epw.location, point.month, point.day, point.hour, north)
+        argument['sky'] = cie_sky(latitude, longitude, point.month,
+                                  point.day, point.hour, north_angle, ground_reflectance)
         arguments.append(argument)
         argument['month_day_hour'] = point.as_string()
 
@@ -96,39 +100,66 @@ def create_job(hbjson_path: Path, api_client: ApiClient, owner: str, project: st
     return new_job
 
 
-def simulation(target_folder: Path, hbjson_path: Path) -> Union[Tuple[str, ApiClient],
-                                                                Tuple[None, None]]:
+def show_map(latitude: float, longitude: float, zoom: float) -> None:
+    """Create a Folium map to show the selected location.
+
+    args:
+        latitude: Latitude of the selected location.
+        longitude: Longitude of the selected location.
+        zoom: Zoom level of the map.
+    """
+
+    m = folium.Map(location=[latitude, longitude], zoom_start=zoom)
+
+    folium.Marker([latitude, longitude], popup="Liberty Bell",
+                  tooltip='Selected location').add_to(m)
+
+    st_folium(m, width=725)
+
+
+def simulation(hbjson_path: Path) -> Union[Tuple[str, ApiClient],
+                                           Tuple[None, None]]:
     """UI for the simulation tab of the Estidama app."""
 
-    st.subheader('Pollination Credentials')
+    latitude = st.number_input('Latitude', value=24.453766,
+                               help='Enter the latitude of the site.')
+    longitude = st.number_input('Longitude', value=54.377375,
+                                help='Enter the longitude of the site.')
 
-    with st.form('pollination-request'):
-        api_key = st.text_input('Enter Pollination API key', type='password')
-        owner = st.text_input('Project owner')
-        project = st.text_input('Project name')
-        study_name = st.text_input('Study name', value='Estidama-daylight')
-        study_description = st.text_input(
-            'Study description', value='PBRS LBi-7 Daylight & Glare compliance')
-        north = st.number_input('North angle', value=0, min_value=0, max_value=360)
-        epw_data = st.file_uploader('Load Abu Dhabi EPW file', type='epw')
+    zoom = st.number_input('Zoom level', value=14)
 
-        submitted = st.form_submit_button('Submit')
+    st.write('Visually check location on the map.')
+    show_map(latitude, longitude, zoom)
 
-        if submitted:
-            if not all([api_key, owner, project, epw_data]):
-                st.error('Make sure to fill all the fields first.')
-                return
+    api_key = st.text_input('Pollination API key', type='password')
+    owner = st.text_input('Project owner')
+    project = st.text_input('Project name')
+    study_name = st.text_input('Study name', value='Estidama-daylight')
+    study_description = st.text_input(
+        'Study description', value='PBRS LBi-7 Daylight & Glare compliance')
 
-            api_client = ApiClient(api_token=api_key)
-            epw = get_epw(epw_data, target_folder)
-            job = create_job(hbjson_path, api_client, owner, project,
-                             study_name, study_description, north, epw)
+    north_angle = st.number_input('North angle', value=0, min_value=0, max_value=360)
+    ground_reflectance = st.number_input(
+        'Ground reflectance', value=0.2, help='Enter a decimal number to indicate'
+        ' the percentage of light being reflected back by the ground.')
 
-            running_job = job.create()
+    submit = st.button('Submit')
 
-            time.sleep(2)
-            job_url = f'https://app.pollination.cloud/{running_job.owner}/projects/{running_job.project}/jobs/{running_job.id}'
-            st.success('Job submitted to Pollination. Move to the next tab.')
-            return job_url, api_client
+    if submit:
+        if not all([api_key, owner, project, latitude, longitude]):
+            st.error('Make sure to fill all the fields first.')
+            return None, None
+
+        api_client = ApiClient(api_token=api_key)
+        job = create_job(hbjson_path, api_client, owner, project,
+                         study_name, study_description, latitude, longitude,
+                         north_angle, ground_reflectance)
+
+        running_job = job.create()
+
+        time.sleep(2)
+        job_url = f'https://app.pollination.cloud/{running_job.owner}/projects/{running_job.project}/jobs/{running_job.id}'
+        st.success('Job submitted to Pollination. Move to the next tab.')
+        return job_url, api_client
 
     return None, None
